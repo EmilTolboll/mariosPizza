797a5abe2c34c6c95c14104f690e678df5eccb37
osman <butt.osman@gmail.com>
2021-10-17 11:33:15 +0200

    Order and OrderLine class basic implementation

--- src/Interface.java
+++ src/Interface.java
@@ -8,9 +8,6 @@ public class Interface {
 
         MenuCard myMenuCardArr = new MenuCard();
 
-
-
-
         myMenuCardArr.printMenuCard();
 
         myMenuCardArr.addPizzaToMenuCard(addPizzaObj);
@@ -23,5 +20,14 @@ public class Interface {
         myMenuCardArr.printMenuCard();
 
         System.out.println(myMenuCardArr.getPizzaPrice(4));
+
+        // Testing orders
+        OrderLine OrderLine1 = new OrderLine(1,1,myMenuCardArr);
+        OrderLine OrderLine2 = new OrderLine(2,1,myMenuCardArr);
+        OrderLine[] OrderLineArr1 = new OrderLine[] {OrderLine1,OrderLine2};
+
+        Order Order1 = new Order(OrderLineArr1,1,"15:00");
+        System.out.println("Price of order 1 is = "+Order1.getTotalPrice());
+
     }
 }
--- src/MenuCard.java
+++ src/MenuCard.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MenuCard<PizzaMenuCardArrList> {
+    public static int getPizza;
     private  Pizza PizzaMenuCard;
     private  Pizza[] PizzaMenuCardArr;
     public   List<Pizza> PizzaMenuCardArrList = new ArrayList <Pizza>();
